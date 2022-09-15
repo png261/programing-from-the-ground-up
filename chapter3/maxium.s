b16575b12cdb2469321c8ba22ed0e05f0e37111f
png261 <nhphuong.code@gmail.com>
2022-09-15 09:37:35 +0700

    chapter3: add maxium.s

---
+++ chapter3/maxium.s
@@ -0,0 +1,36 @@
+#
+# PURPOSE: This program finds the maximum number of a set of data items.
+#
+# VARIABLES: The registers have the following uses:
+# %edi - index
+# %ebx - largest number
+# %eax - current number
+#
+# The following memory locations are used:
+# data_items - contains the item data. A 0 is used to terminate the data
+#
+
+.section .data
+data_items:
+    .long 3,67,34,222,45,75,54,34,44,33,22,11,66,0
+
+.section .text
+.globl _start
+_start:
+    movl $0, %edi
+    movl data_items(,%edi, 4), %eax
+    movl %eax, %ebx
+    
+start_loop:
+    cmpl $0, %eax
+    je loop_exit
+    incl %edi
+    movl data_items(,%edi, 4), %eax
+    cmpl %ebx, %eax
+    jle start_loop
+    movl %eax, %ebx
+    jmp start_loop
+
+loop_exit:
+    movl $1, %eax
+    int $0x80
